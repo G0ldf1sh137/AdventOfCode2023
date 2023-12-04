994d879b0a27982f7080369ae2e60227c98d5fea
Matt <m.rich137@gmail.com>
2023-12-04 10:33:22 -0500

    completed 2015 day-02 p2

--- 2015/day-02/p2.py
+++ 2015/day-02/p2.py
@@ -4,17 +4,18 @@ floor_count = 0
 with open("./input.txt") as INPUT:
   gifts = INPUT.read().splitlines()
 
-def getSurfaceArea(l:int, w:int, h:int):
-  return 2*(l*w + w*h + h*l)
+def getMinPerimeter(l:int, w:int, h:int):
+  return 2*min(l+w, w+h, h+l)
 
-def getSlack(l:int, w:int, h:int):
-  return min(l*w, w*h, h*l)
+def getVolume(l:int, w:int, h:int):
+  return l*w*h
 
 result = 0
 
 for gift in gifts:
   l,w,h = gift.split('x')
-  sf = getSurfaceArea(int(l),int(w),int(h)) + getSlack(int(l),int(w),int(h))
-  result += sf
+  ribbon = getMinPerimeter(int(l),int(w),int(h)) + getVolume(int(l),int(w),int(h))
+  # print(ribbon)
+  result += ribbon
 
 print(result)
