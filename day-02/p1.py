d4d45302b425ee6b2cde2637906e28f869756a25
G0ldf1sh137 <m.rich137@gmail.com>
2023-12-04 15:48:58 -0500

    completed part 1

---
+++ day-02/p1.py
@@ -0,0 +1,40 @@
+INPUT_PATH = './input.txt'
+MAX_COUNT = {
+  'red': 12,
+  'green': 13,
+  'blue': 14
+}
+result = 0
+
+with open(INPUT_PATH) as INPUT_DATA:
+  lines = INPUT_DATA.readlines()
+
+def checkPulls(pulls: str) -> bool:
+  # print(pulls.strip())
+  pull = pulls.split(',')
+  for colors in pull:
+    # print(colors.strip())
+    count_color = colors.split()
+    # print(count_color)
+    if int(count_color[0]) > MAX_COUNT[count_color[1]]:
+      print("NOT POSSIBLE")
+      return False
+  return True
+
+def parseLine(line: str) -> int:
+  game_end = line.find(':')
+  game_id = int(line[5:game_end])
+  print('ID:', game_id)
+  pulls = line[game_end+2:].split(';')
+  valid = True
+  for pull in pulls:
+      valid = checkPulls(pull)
+      if not valid:
+        return 0
+  return game_id
+
+# parseLine(lines[0])
+for line in lines:
+  result += parseLine(line)
+
+print(result)
