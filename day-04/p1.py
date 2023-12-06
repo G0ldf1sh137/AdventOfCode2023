94bc8a56999f84521b10963318a8c4a1e2fb3f7c
G0ldf1sh137 <m.rich137@gmail.com>
2023-12-06 18:46:52 -0500

    day 4, p1 cleanup

--- day-04/p1.py
+++ day-04/p1.py
@@ -1,23 +1,17 @@
 INPUT_PATH = './input.txt'
-GAME_START = 10
-GAME_END = 40
-
-WINNING_START = 42
+GAME_START = 10     # test is 8, input is 10
+GAME_END = 40       # test is 23, input is 40
+WINNING_START = 42  # test is 25, input is 42
 
 cards = []
+result = 0
 
 with open(INPUT_PATH) as INPUT_DATA:
   cards = INPUT_DATA.read().splitlines()
 
-result = 0
-
 for card in cards:
   card_numbers = card[GAME_START:GAME_END].split()
-  # print(card_numbers)
-
   winning_numbers = card[WINNING_START:].split()
-  # print(winning_numbers)
-
   card_total = 0
 
   for num in card_numbers:
