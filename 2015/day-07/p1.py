9e083be2f7b7f617de48d5457359878341498d28
G0ldf1sh137 <m.rich137@gmail.com>
2023-12-11 13:54:41 -0500

    2015 d07 create class to parse input

--- 2015/day-07/p1.py
+++ 2015/day-07/p1.py
@@ -1,18 +1,66 @@
 INPUT_FILE = './test.txt'
 
-node = dict()
-
 class Node:
     def __init__(self, txt: str) -> None:
+        self.val = None
         split_idx = txt.find('->')
         self.connection = txt[:split_idx - 1].split()
         if len(self.connection) == 1:
-            self.val = int(self.connection[0])
+            self._identity(self.connection[0])
+        elif self.connection[1] == 'AND':
+            self._and(self.connection[0], self.connection[2])
+        elif self.connection[1] == 'OR':
+            self._or(self.connection[0], self.connection[2])
+        elif self.connection[1] == 'LSHIFT':
+            self._lshift(self.connection[0])
+        elif self.connection[1] == 'RSHIFT':
+            self._rshift(self.connection[0])
+        elif self.connection[0] == 'NOT':
+            self._not(self.connection[1])
         else:
             self.val = None
 
     def __str__(self) -> str:
         return f'{self.connection} = {self.val}'
+    
+    def _identity(self, n1: str):
+        if n1.isdecimal():
+            self.val = int(self.connection[0])
+        else:
+            self.val = nodes[n1].val
+    
+    def _and(self, n1: str, n2: str):
+        if n1 in nodes and nodes[n1].val and n2 in nodes and nodes[n2].val:
+            self.val = nodes[n1].val & nodes[n2].val
+        else:
+            self.val = None
+
+    def _or(self, n1: str, n2: str):
+        if n1 in nodes and nodes[n1].val and n2 in nodes and nodes[n2].val:
+            self.val = nodes[n1].val | nodes[n2].val
+        else:
+            self.val = None
+
+    def _lshift(self, n1: str):
+        if n1 in nodes and nodes[n1].val:
+            self.val = nodes[n1].val << int(self.connection[2])
+        else:
+            self.val = None
+
+    def _rshift(self, n1: str):
+        if n1 in nodes and nodes[n1].val:
+            self.val = nodes[n1].val >> int(self.connection[2])
+        else:
+            self.val = None
+    
+    def _not(self, n1: str):
+        if n1 in nodes and nodes[n1].val:
+            self.val = ~ nodes[n1].val
+        else:
+            self.val = None
+
+
+nodes: dict[str, Node] = dict()
 
 with open(INPUT_FILE) as DATA:
     file_data = DATA.read().splitlines()
@@ -20,6 +68,6 @@ with open(INPUT_FILE) as DATA:
 for n in file_data:
     split_idx = n.rfind(' ')
     node_key = n[split_idx + 1:]
-    node[node_key] = Node(n)
+    nodes[node_key] = Node(n)
 
-    print(node_key, '=', node[node_key])
+    print(node_key, '=', nodes[node_key])
